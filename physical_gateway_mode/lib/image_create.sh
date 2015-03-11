b162f24f6a85b2e608bf7c83e9dac7bc76ea9c17
wangtaoking1 <wangtaoking1@gmail.com>
2015-03-11 17:13:21 +0800

    update iamge_create.sh

--- physical_gateway_mode/lib/image_create.sh
+++ physical_gateway_mode/lib/image_create.sh
@@ -13,7 +13,7 @@ set -o xtrace
 
 # Download the Cirros Image for test
 wget 222.200.185.40:8000/img/cirros-0.3.0-x86_64-disk.img
-mv cirros-0.3.0-x86_64-disk.img ./packages/
+
 
 # Create image
-glance --os-username admin --os-password $ADMIN_PASSWORD --os-tenant-name admin --os-auth-url http://$MANAGEIP:5000/v2.0/ image-create --name='cirros' --public --container-format=ovf --disk-format=qcow2 < ./packages/cirros-0.3.0-x86_64-disk.img
+glance --os-username admin --os-password $ADMIN_PASSWORD --os-tenant-name admin --os-auth-url http://$MANAGEIP:5000/v2.0/ image-create --name='cirros' --public --container-format=ovf --disk-format=qcow2 < ./cirros-0.3.0-x86_64-disk.img
